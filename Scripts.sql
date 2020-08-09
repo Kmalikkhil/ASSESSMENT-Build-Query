ffc6cb7ab6f0ce579843071b4cc998f194343c30
Kanishka Malikkhil <103220762@student.swin.edu.au>
2020-08-09 20:19:51 +1000

    Script file added

---
+++ Scripts.sql
@@ -0,0 +1,34 @@
+/*
+
+Name: Kanishka Malikkhil
+Student_ID: 103220762
+
+*/
+
+/*
+---- TASK 1 ----
+
+
+
+
+*/
+
+--- Task 2 ---
+
+
+--- Task 3 ---
+
+
+
+--- Task 4 ---
+
+
+
+
+--- Task 5 ---
+
+
+
+
+--- Task 6 ---
+
